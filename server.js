5816f201ca5daed7fca760967fdbadf572b4e404
cee breezy <djmisscbrown@hotmail.com>
2022-03-09 20:04:35 +0000

    Attempt to fix js bug

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 // Requirements
 const express = require('express'); 
+const path = require('path')
 const apiRouter = require('./routes/apiRoutes')
 const htmlRouter = require('./routes/htmlRoutes'); 
+
 
 // Initialise the app and assign PORT variable
 const app = express();
@@ -10,7 +12,7 @@ const PORT = process.env.PORT || 3001;
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({extended: true})); 
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, './public'))); 
 
 app.use('/api', apiRouter)
 app.use('/', htmlRouter)
