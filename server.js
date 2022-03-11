2a2e541eaa2781ad83f2a6cdccde07fbf44f5cfb
cee breezy <djmisscbrown@hotmail.com>
2022-03-11 19:57:21 +0000

    Addressed bug as server not loading

--- server.js
+++ server.js
@@ -1,23 +1,26 @@
 // Requirements
-const express = require('express'); 
-const path = require('path')
-const apiRouter = require('./routes/apiRoutes')
-const htmlRouter = require('./routes/htmlRoutes'); 
 
-
+const express = require("express");
+const fs = require("fs");
+const notes = require("./db/store");
+const path = require("path");
+const apiRouter = require("./routes/apiRoutes");
+const htmlRouter = require("./routes/htmlRoutes");
+// console.log(htmlRouter);
 // Initialise the app and assign PORT variable
 const app = express();
-const PORT = process.env.PORT || 3001; 
-
+const PORT = process.env.PORT || 3001;
 // Middleware
 app.use(express.json());
-app.use(express.urlencoded({extended: true})); 
-app.use(express.static('/public')); 
-
-app.use('/api', apiRouter)
-app.use('/', htmlRouter)
-
- // Start the app on the given port
- app.listen(PORT, () => {
-     console.log('App listening.....')
- })
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("/public"));
+app.use("/api", apiRouter);
+app.use("/", htmlRouter);
+// app.use("/api/notes", notes);
+// app.get("*", (request, response) => {
+//   response.sendFile(path.join(__dirname, "../public/index.html"));
+// });
+// Start the app on the given port
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}......`);
+});
